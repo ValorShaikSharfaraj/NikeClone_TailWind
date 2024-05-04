09d219b186116680f0842282b030e91bd4066428
Shaik SHarfaraj Ahammed <codewithsharfaraj13@gmail.com>
2024-05-04 18:03:31 +0530

    Created App page structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,34 @@ import React from 'react'
 
 export default function App() {
   return (
-    <h1 className='text-3xl font-bold text-red-600'>Hello World</h1>
+    <main className='relative'>
+      Nav
+      <section className='xl:padding-1 wide:padding-r
+      padding-b'>
+        Hero
+      </section>
+
+      <section className='padding'>
+        Popular Products
+      </section>
+      <section className='padding'>
+        Super Quality
+      </section>
+      <section className='padding-x py-10'>
+        Services
+      </section>
+      <section className='padding'>
+        Special Offer
+      </section>
+      <section className='bg-pale-blue padding'>
+        Customer Reviews
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        Subscribe
+      </section>
+      <section className='bg-black padding-x padding-t pb-8'>
+        footer
+      </section>
+    </main>
   )
 }
